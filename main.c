c09eee4abecc2f7679f8b21ecec2aea3bbebf61a
Julia <juliafryklind@gmail.com>
2025-08-14 16:12:59 +0200

    stackoverflow i love you

--- main.c
+++ main.c
@@ -70,8 +70,8 @@ bool checkIndex(int user_index)
 // calculates new index from userindex
 Index calcIndex(Index currIndex, int numIndex)
 {
-    currIndex.row = (int)((numIndex-1) / WIDTH);
-    currIndex.col = (int)((numIndex-1) % WIDTH);
+    currIndex.row = (int)((numIndex - 1) / WIDTH);
+    currIndex.col = (int)((numIndex - 1) % WIDTH);
     return currIndex;
 }
 
@@ -81,12 +81,94 @@ void changeTile(Playfield *pf, Index tileIndex, char token)
     pf->pf_cells[tileIndex.row][tileIndex.col] = token;
 }
 
-Player * getNextPlayer(Player *player1, Player *player2, int counter)
+Player *getNextPlayer(Player *player1, Player *player2, int counter)
 {
-    //if divisable by 2 return player 1, otherwise player2
+    // if divisable by 2 return player 1, otherwise player2
     return (counter % 2 == 0) ? player1 : player2;
 }
 
+bool checkRow(Playfield *pf, char token)
+{
+
+    for (int r = 0; r < WIDTH; r++)
+    {
+        bool allSame = true;
+
+        for (int c = 0; c < WIDTH; c++)
+        {
+            if (pf->pf_cells[r][c] != token)
+            {
+                allSame = false;
+                break;
+            }
+        }
+        if (allSame)
+            return true;
+    }
+    return false;
+}
+
+bool checkCol(Playfield *pf, char token)
+{
+    for (int c = 0; c < WIDTH; c++)
+    {
+        bool allSame = true;
+
+        for (int r = 0; r < WIDTH; r++)
+        {
+            if (pf->pf_cells[r][c] != token)
+            {
+                allSame = false;
+                break;
+            }
+        }
+        if (allSame) return true;
+    }
+    return false;
+}
+
+bool checkDig(Playfield *pf, char token)
+{
+    bool allSame = true;
+    for (int i = 0; i < WIDTH; i++)
+    {
+        if (pf->pf_cells[i][i] != token)
+        {
+            allSame = false;
+            break;
+        }
+    }
+    if(allSame) return true;
+
+
+
+    // anti-diagonal
+    allSame = true;
+    for (int i = 0; i < WIDTH; i++) {
+        if (pf->pf_cells[i][WIDTH - 1 - i] != token) {
+            allSame = false;
+            break;
+        }
+    }// anti-diagonal
+    allSame = true;
+    for (int i = 0; i < WIDTH; i++) {
+        if (pf->pf_cells[i][WIDTH - 1 - i] != token) {
+            allSame = false;
+            break;
+        }
+    }
+    if (allSame) return true;
+
+    return false;
+    if (allSame) return true;
+
+    return false;
+
+}
+bool isThereAWinner(Playfield *pf, char token)
+{
+    return checkRow(pf, token) || checkCol(pf, token) || checkDig(pf, token);
+}
 // PLS C gods make my code run <33
 
 int main()
@@ -95,7 +177,7 @@ int main()
     // starter things
     Player player1 = {1, 'X', 0};
     Player player2 = {2, 'O', 0};
-    Player * currentPlayer=&player1; //a Player pointer, may point to player 1 or player 2
+    Player *currentPlayer = &player1; // a Player pointer, may point to player 1 or player 2
 
     Playfield pf;
     int counter = 0;
@@ -106,15 +188,23 @@ int main()
     resetPlayfield(&pf);
 
     while (gameIsRunning)
-    {   currentPlayer=getNextPlayer(&player1,&player2,counter);
+    {
+        currentPlayer = getNextPlayer(&player1, &player2, counter);
         printPlayfield(&pf);
         printf("It is player %d:s turn.", currentPlayer->num);
         printf(" Enter an index between 1-9:");
         scanf("%d", &index_1d);
-        currIndex=calcIndex(currIndex,index_1d);
-        changeTile(&pf,currIndex,currentPlayer->token);
-
-
+        if (!checkIndex(index_1d))
+        {
+            printf(" Enter a valid index between 1-9:");
+            scanf("%d", &index_1d);
+        }
+        currIndex = calcIndex(currIndex, index_1d);
+        changeTile(&pf, currIndex, currentPlayer->token);
+        if (isThereAWinner(&pf, currentPlayer->token))
+        {
+            printf("Player %d:s has won.", currentPlayer->num);
+        }
         counter++;
     }
 
